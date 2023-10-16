bb17f83e417af77735e8bf81cc2414cac251cd75
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-10-16 23:22:26 +0300

    added auth routes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use("/users", authRouter);
+app.use("/auth", authRouter);
 app.use("/reviews", reviewsRouter);
 
 app.use((req, res) => {
