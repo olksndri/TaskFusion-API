71fceb82caa70bf945ed2363a4ff032d3f2070ce
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-10-19 11:09:34 +0300

    Changed login password validation

--- joi_schemas/users-schemas.js
+++ joi_schemas/users-schemas.js
@@ -8,15 +8,11 @@ const registerSchema = Joi.object({
 
   email: Joi.string()
     .pattern(emailRegexp)
-    // .email({
-    //   minDomainSegments: 2,
-    //   tlds: { allow: ["com", "net"] },
-    // })
     .required(),
 });
 
 const loginSchema = Joi.object({
-  password: Joi.string().min(6).pattern(passRegexp).required(),
+  password: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
 });
 
